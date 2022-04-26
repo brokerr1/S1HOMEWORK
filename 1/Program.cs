7e99c8f8455d996bbaec2a688580197d46dd7dce
brokerr1 <solovievdima1@gmail.com>
2022-04-26 16:03:41 +0300

    task1

--- 1/Program.cs
+++ 1/Program.cs
@@ -1,6 +1,6 @@
 ﻿Console.Write("Введите первое число: ");
 int numberA = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите первое число: ");
+Console.Write("Введите второе число: ");
 int numberB = Convert.ToInt32(Console.ReadLine());
 Console.Write("Максимальное число: ");
 if (numberA > numberB)
