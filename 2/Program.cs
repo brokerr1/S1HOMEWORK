be0dd47e9abe532e85ea79fdc3fd761414f33509
brokerr1 <solovievdima1@gmail.com>
2022-04-26 16:14:24 +0300

    task2

--- 2/Program.cs
+++ 2/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите первое число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите третье число: ");
+int numberС = Convert.ToInt32(Console.ReadLine());
+int numberMax = numberA;
+if (numberB > numberMax)
+{
+    numberMax = numberB; 
+}
+if (numberС > numberMax)
+{
+    numberMax = numberС;
+}
+Console.Write("Максимальное число: ");
+Console.WriteLine(numberMax);
+
+
