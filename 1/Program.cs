6b89b31d019736928e907e3a171554e63a3a4853
brokerr1 <solovievdima1@gmail.com>
2022-04-26 17:59:07 +0300

    task1 coment

--- 1/Program.cs
+++ 1/Program.cs
@@ -1,4 +1,9 @@
-﻿Console.Write("Введите первое число: ");
+﻿/*
+Напишите программу, которая на вход
+принимает два числа и выдаёт, какое число большее, а
+какое меньшее.
+*/
+Console.Write("Введите первое число: ");
 int numberA = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите второе число: ");
 int numberB = Convert.ToInt32(Console.ReadLine());
